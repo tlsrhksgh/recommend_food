06406084bc11ac54592751e85c01814feae25743
tlsrhksgh <sin99145@naver.com>
2023-05-20 18:39:55 +0900

    feature: Jwt 로그인 인증 기능 구현 완료.

---
+++ user-api/src/main/java/com/single/user/service/MemberService.java
@@ -0,0 +1,37 @@
+package com.single.user.service;
+
+import com.single.user.domain.model.Member;
+import com.single.user.domain.repository.MemberRepository;
+import com.single.user.exception.ErrorCode;
+import com.single.user.exception.MemberException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static com.single.user.exception.ErrorCode.*;
+
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Optional<Member> findByEmail(String email) {
+        return memberRepository.findByEmail(email);
+    }
+
+    public Member findValidCustomer(String email, String password) {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new MemberException(MEMBER_NOT_FOUND));
+
+        if(!member.getEmail().equals(email) && member.isVerify()) {
+            throw new MemberException(LOGIN_MAIL_CHECK_FAIL);
+        } else if(!member.getPassword().equals(password) && member.isVerify()) {
+            throw new MemberException(LOGIN_PASSWORD_CECK_FAIL);
+        } else if(member.getEmail().equals(email) && member.getPassword().equals(password) && !member.isVerify()) {
+            throw new MemberException(NOT_VERIFIED_EMAIL_CODE);
+        }
+
+        return member;
+    }
+}
